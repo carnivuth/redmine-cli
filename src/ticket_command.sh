a914da73c920846d3ae2d9b82599aa7a6d819409
carnivuth <mlonghi@labs.it>
2024-10-29 18:07:57 +0100

    first commit

---
+++ src/ticket_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/ticket_command.sh'"
+echo "# code for 'redmine-cli ticket' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
