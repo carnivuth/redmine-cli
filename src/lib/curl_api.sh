b67d6e68b12e4ebaa2f41bdfc0fe170903a2a57b
carnivuth <mlonghi@labs.it>
2024-10-30 12:44:27 +0100

    added dev environment and basic auth

--- src/lib/curl_api.sh
+++ src/lib/curl_api.sh
@@ -1,17 +1,7 @@
-## Add any function here that is needed in more than one parts of your
-## application, or that you otherwise wish to extract from the main function
-## scripts.
-##
-## Note that code here should be wrapped inside bash functions, and it is
-## recommended to have a separate file for each function.
-##
-## Subdirectories will also be scanned for *.sh, so you have no reason not
-## to organize your code neatly.
-##
 curl_api() {
   path="$1"
   method="$2"
-  params="-s $REDMINE_ADDRESS/$path -X $method"
+  params="-s $REDMINE_ADDRESS/$path -X $method -u "$REDMINE_USERNAME:$REDMINE_PASSWORD""
   # check for data
   if [[ "$3" != '' ]];then
     params="$params -d $3"
